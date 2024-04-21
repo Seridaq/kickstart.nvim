983dcd5c1f9527c061999ebf7b2c586224524cf3
Thomas Bruhnke <tebruhnke@gmail.com>
2024-04-21 13:19:57 -0500

    updates

--- init.lua
+++ init.lua
@@ -472,7 +472,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'ocaml', },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -640,7 +640,6 @@ local servers = {
   rust_analyzer = {},
   -- tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
-  ocamllsp = {},
   csharp_ls = {},
   lua_ls = {
     Lua = {
